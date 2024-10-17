6cc73a1744993a7f453add61a166f034612210a2
KirillFI08 <kirillfischer08@gmail.com>
2024-10-17 20:06:01 +0200

    17.10 (3)

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person {
+
+    public Person(String name, int alter){
+        this.name = name;
+        this.alter =alter;
+    }
+    public void introduceYourself(){
+        System.out.println("Hallo mein Name ist "+ name+ " und ich bin "+alter+" Jahre alt");
+    }
+    
+}
