a0b486aca5e387c2b06825da3353d3bb8f6727a0
KirillFI08 <kirillfischer08@gmail.com>
2025-03-15 18:27:52 +0100

    15.03 (2)

--- ElektrAuto/Test/test.java
+++ ElektrAuto/Test/test.java
@@ -1,23 +1,35 @@
 public class test {
-    public static void main(String[] args){
-        sAuf(3);
+    public static void main(String[] args) {
+        int c = 20;
+        int d = c + 1;
 
-
-    }
-
-    public static void sAuf(int c){
-        if(c<1){ 
-            System.out.println("to low"); 
-        }
-        if(0<c){
-            for(int i = 0; i<c; i++){
-            System.out.println("*");
-        }
-        sAuf(c-1);
-
-
+        sAuf(c, d);
+        sAb(c);
 
     }
 
-}
+    public static void sAuf(int c, int d) {
+
+        System.out.println(" ");
+        if (0 < c) {
+            for (int i = 0; i < d - c; i++) {
+                System.out.print("*");
+            }
+            sAuf(c - 1, d);
+        }
+
+    }
+
+    public static void sAb(int c) {
+
+        if (0 < c) {
+            for (int i = 1; i < c; i++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+            sAb(c - 1);
+        }
+
+    }
 }
